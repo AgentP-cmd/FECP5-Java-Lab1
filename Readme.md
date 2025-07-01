db0427206ca001c2ee717f231ce682297b98ab5c
Harold Bryan Santos <harold.santos@GXCHLAP00690.local>
2025-07-01 15:04:53 +0800

    Final Submission, Lab 1: Celsius to Fahrenheit Converter

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# HAROLD BRYAN SANTOS
+***
+### Lab 1: Celsius to Fahrenheit Converter
+![image info](Screenshot 2025-07-01 at 3.02.10 PM.png)
+
+### Output:
+![image info](Screenshot 2025-07-01 at 3.02.56 PM.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
+import java.util.*;
 
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int cel;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
+        cel = sc.nextInt();
+
+        double fah = cel * (1.8)+ 32;
+        System.out.printf("Temperature in Fahrenheit: %.2f", fah);
 
     }
 }
